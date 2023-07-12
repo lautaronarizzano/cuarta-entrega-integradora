528660049a9e8a9c0b8751b4d49fee9901b87a8f
Lautaro Narizzano <lautaronarizzano@gmail.com>
2023-07-11 23:27:43 -0300

    solucionado

--- src/dao/factory.js
+++ src/dao/factory.js
@@ -1,29 +1,42 @@
 import mongoose from "mongoose"
+import config from "../config/config.js"
 
 export let Users
 export let Products
 export let Carts
 
-const persistence = process.env.PERSISTENCE
+const persistence = config.persistence
 
-const mongoUrl = process.env.MONGO_URL
+const mongoUrl = config.mongoUrl
 
 switch (persistence) {
     case "MONGO":
         console.log("Usando DAO de mongo")
         await mongoose.connect(mongoUrl)
-    const { default: ProductsMongo } = await import('./dbManagers/products.js')
-    const { default: CartsMongo } = await import('./dbManagers/carts.js')
-    const { default: UsersMongo } = await import('./dbManagers/users.js')
-    Products = ProductsMongo
-    Carts = CartsMongo
-    Users = UsersMongo
+        const {
+            default: ProductsMongo
+        } = await import('./dbManagers/products.js')
+        console.log('este es' + ProductsMongo)
+        const {
+            default: CartsMongo
+        } = await import('./dbManagers/carts.js')
+        const {
+            default: UsersMongo
+        } = await import('./dbManagers/users.js')
+        Products = ProductsMongo
+        Carts = CartsMongo
+        Users = UsersMongo
         break
     case "FILE":
         console.log('Usando DAO de file')
-        const { default: ProductsFile } = await import('./fileManagers/ProductManager.js')
-        const { default: CartsFile } = await import('./fileManagers/CartsManager.js')
+        const {
+            default: ProductsFile
+        } = await import('./fileManagers/ProductManager.js')
+        const {
+            default: CartsFile
+        } = await import('./fileManagers/CartsManager.js')
         Products = ProductsFile
         Carts = CartsFile
         break
-        }
+}
+
